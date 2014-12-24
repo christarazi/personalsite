a58dd69e6b973d723685e623a1f61e97ddef89b6
christarazi <christarazi@users.noreply.github.com>
2014-12-24 11:24:51 -0800

    Update settings.py

--- TaraziChrisSite/TaraziChrisSite/settings.py
+++ TaraziChrisSite/TaraziChrisSite/settings.py
@@ -81,7 +81,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/ctarazi/static/'
 
 TEMPLATE_DIRS = [
     os.path.join(BASE_DIR, 'TaraziChrisSite/templates')
