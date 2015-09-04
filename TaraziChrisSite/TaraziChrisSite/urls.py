84fb5ca77fb6706a6d88b6fba17483d8f97bb03f
Chris Tarazi <chris.tarazi@yahoo.com>
2015-09-04 14:31:15 -0700

    Disallow caching of static files

--- TaraziChrisSite/TaraziChrisSite/urls.py
+++ TaraziChrisSite/TaraziChrisSite/urls.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 
 from django.views.generic import TemplateView
 
+# http://stackoverflow.com/a/11369944
+from django.contrib.staticfiles.views import serve
+from django.views.decorators.cache import never_cache
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'TaraziChrisSite.views.home', name='home'),
@@ -13,3 +17,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^fortune/', include('fortune.urls')),
 )
+
+static_view = never_cache(serve)
+urlpatterns += static_view(settings.MEDIA_URL,
+                           document_root=settings.MEDIA_ROOT)
